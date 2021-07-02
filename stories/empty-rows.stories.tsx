f5034012a3ca63f8d5cfabff34f4882202b885aa
jhonebee <zhangjin19960908@gamil.com>
2021-07-02 16:37:58 +0800

    feat: 添加 onEmptyRowsRenderer 方法支持自定义渲染空数据显示的内容

---
+++ stories/empty-rows.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+
+import DataGrid, { Row, Column } from '../src'
+
+const rows: Array<Row> = []
+const columns: Array<Column<unknown>> = []
+
+columns.push({
+    name: `0`,
+    title: `字段 - 0`,
+    fixed: 'left',
+})
+
+for (let i = 2; i < 1000; i += 1) {
+    columns.push({
+        name: `${i}`,
+        title: `字段 - ${i}`,
+    })
+}
+
+columns.push({
+    name: `1`,
+    title: `字段 - 1`,
+    fixed: 'right',
+})
+
+const RowDataGrid = () => (
+    <DataGrid<unknown>
+        rows={rows}
+        columns={columns}
+        onEmptyRowsRenderer={() => (
+            <div
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    position: 'absolute',
+                    top: 0,
+                }}
+            >
+                {' '}
+                <br /> <br />
+                无任何数据{' '}
+            </div>
+        )}
+    />
+)
+
+export default {
+    component: RowDataGrid,
+    title: 'Demos',
+} as Meta
+
+export const EmptyRows: React.VFC<{}> = () => <RowDataGrid />
