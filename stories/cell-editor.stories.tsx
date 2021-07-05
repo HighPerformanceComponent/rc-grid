21132bc8e5af936dd0a6701214d3c5c39ca51fc0
jhonebee <zhangjin19960908@gamil.com>
2021-07-05 08:46:04 +0800

    feat: 修复 eslint 在 CI中执行的问题

--- stories/cell-editor.stories.tsx
+++ stories/cell-editor.stories.tsx
@@ -4,75 +4,68 @@ import { Meta } from '@storybook/react'
 import styled from 'styled-components'
 import DataGrid, { Row, Column, Cell, EditorProps } from '../src'
 
-
 const rows: Array<Row<any>> = []
 
-const Input = ({
-    style,
-    value,
-    onEditCompleted,
-    onChange
-}: EditorProps) => {
-    return (
-        <input
-            autoFocus
-            style={style}
-            value={value as string}
-            onBlur={() => {
-                onEditCompleted()
-            }}
-            onChange={(e) => {
-                const text = e.target.value
-                onChange(text)
-            }}
-        />
-    )
-}
+const Input = ({ style, value, onEditCompleted, onChange }: EditorProps) => (
+    <input
+        // eslint-disable-next-line jsx-a11y/no-autofocus
+        autoFocus
+        style={style}
+        value={value as string}
+        onBlur={() => {
+            onEditCompleted()
+        }}
+        onChange={(e) => {
+            const text = e.target.value
+            onChange(text)
+        }}
+    />
+)
 const columns: Array<Column<unknown>> = [
     {
         name: `0`,
         title: `姓名`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `1`,
         title: `年龄`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `2`,
         title: `身份证号`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `3`,
         title: `家庭地址`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `4`,
         title: `家庭电话号码`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `5`,
         title: `家庭人员数量`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `6`,
         title: `公司名称`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `7`,
         title: `公司地址`,
-        editor: Input
+        editor: Input,
     },
     {
         name: `8`,
         title: `公司电话号码`,
-        editor: Input
+        editor: Input,
     },
 ]
 
@@ -125,7 +118,6 @@ for (let i = 0; i < 5000; i += 1) {
 
 const RowDataGrid = () => (
     <DataGrid<unknown>
-        
         rows={rows}
         columns={columns}
         onHeaderRowRender={(node) => {
@@ -147,23 +139,22 @@ const RowDataGrid = () => (
             if (index === 0) {
                 return [
                     <GridHeaderCell
-                            key="merge-header-cell"
-                            style={{
-                                width: 120 * 6,
-                                height: 35,
-                                position: 'absolute',
-                                display: 'inline-flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                zIndex: 100,
-                                left: tempStyled.left,
-                                top: (tempStyled.top || 0) - 35,
-                            }}
-                        >
+                        key="merge-header-cell"
+                        style={{
+                            width: 120 * 6,
+                            height: 35,
+                            position: 'absolute',
+                            display: 'inline-flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            zIndex: 100,
+                            left: tempStyled.left,
+                            top: (tempStyled.top || 0) - 35,
+                        }}
+                    >
                         人员资料
-                    </GridHeaderCell>
-                    ,
-                    headerCell
+                    </GridHeaderCell>,
+                    headerCell,
                 ]
             }
 
@@ -176,7 +167,7 @@ const RowDataGrid = () => (
                             top: -35,
                             height: 35 * 2,
                         },
-                    })
+                    }),
                 ]
             }
             return [
@@ -185,7 +176,7 @@ const RowDataGrid = () => (
                     styled: {
                         ...tempStyled,
                     },
-                })
+                }),
             ]
         }}
     />
