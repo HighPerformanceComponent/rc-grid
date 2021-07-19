a995b1c29b64249c58924af792919d4f248181a9
zhangj <zhangjin19960908@gmail.com>
2021-07-19 17:33:02 +0800

    docs: 添加 Demo 到文档中 (#104)
    * docs: 添加 Demo 到文档中
    
    * docs: 添加 Demo 到文档中
    
    Co-authored-by: jhonebee <zhangjin19960908@gamil.com>

---
+++ website/docs/getting-started/example/empty-rows-example.zh-CN.md
@@ -0,0 +1,13 @@
+---
+nav:
+  title: Docs
+  order: -1
+group:
+  title: 例子
+title: 空数据显示
+order: 5
+---
+
+# 空数据显示
+
+<code src="../../../src/empty-rows.tsx" title="空数据显示" desc="在空数据的时候可以通过 `onEmptyRowsRenderer` 自定义显示内容" />
---
+++ website/src/expandable-row.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import DataGrid, { Row, Column, Cell, AutoSize } from '@lifhcp/rc-grid'
+
+const rows: Array<Row<any>> = []
+const tempColumns: Array<Column<unknown>> = []
+
+tempColumns.push({
+    name: `0`,
+    title: `字段 - 0`,
+    fixed: 'left',
+})
+
+for (let i = 2; i < 20; i += 1) {
+    tempColumns.push({
+        name: `${i}`,
+        title: `字段 - ${i}`,
+    })
+}
+
+tempColumns.push({
+    name: `1`,
+    title: `字段 - 1`,
+    fixed: 'right',
+})
+
+for (let i = 0; i < 500; i += 1) {
+    const cells: Array<Cell> = []
+
+    const object: any = {}
+    for (let y = 0; y < tempColumns.length; y += 1) {
+        object[`${y}`] = `${i} - ${y}`
+        if (i === 3 && y === 2) {
+            cells.push({
+                name: `${y}`,
+                value: `${i} - ${y}`,
+                style: {},
+            })
+        } else if (i === 8 && y === 2) {
+            cells.push({
+                name: `${y}`,
+                value: `${i} - ${y}`,
+                style: {},
+            })
+        } else {
+            cells.push({
+                name: `${y}`,
+                value: `${i} - ${y}`,
+            })
+        }
+    }
+    rows.push({
+        height: 35,
+        key: `${i}`,
+        object,
+        cells,
+    })
+}
+
+const RowExpandableDataGrid = () => (
+    <AutoSize>
+        {(width) => (
+            <DataGrid<unknown>
+                rows={rows}
+                width={width}
+                height={500}
+                columns={tempColumns}
+                expandable={{
+                    expandedRowRender: (row, style) => (
+                        <div style={style}>
+                            {' '}
+                            这是一个展开的内容信息 {JSON.stringify(row)}{' '}
+                        </div>
+                    ),
+                }}
+            />
+        )}
+    </AutoSize>
+)
+
+export default RowExpandableDataGrid
