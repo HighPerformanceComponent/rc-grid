083e0dee2e91e8dc9332b22b955f221225d41214
zhangj <zhangjin19960908@gmail.com>
2021-07-21 10:36:08 +0800

    docs: 添加列的可调整大小 (#109)
    Co-authored-by: jhonebee <zhangjin19960908@gamil.com>

---
+++ website/docs/getting-started/example/header-resizable.zh-CN.md
@@ -0,0 +1,13 @@
+---
+nav:
+  title: Docs
+  order: -1
+group:
+  title: 例子
+title: 列的宽度可调整
+order: 8
+---
+
+# 列的宽度可调整
+
+<code src="../../../src/header-resizable.tsx" title="列的宽度可调整" desc="可改变表格列的宽度信息" />
---
+++ website/src/header-resizable.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+
+import DataGrid, { Row, Column, Cell, AutoSize } from '@lifhcp/rc-grid'
+
+const rows: Array<Row<any>> = []
+const tempColumns: Array<Column<unknown>> = []
+
+tempColumns.push({
+    name: `0`,
+    title: `字段 - 0`,
+    fixed: 'left',
+})
+
+for (let i = 2; i < 20; i += 1) {
+    tempColumns.push({
+        name: `${i}`,
+        title: `字段 - ${i}`,
+        resizable: true,
+        sort: true,
+    })
+}
+
+tempColumns.push({
+    name: `1`,
+    title: `字段 - 1`,
+    fixed: 'right',
+})
+
+for (let i = 0; i < 500; i += 1) {
+    const cells: Array<Cell> = []
+
+    const object: any = {}
+    for (let y = 0; y < tempColumns.length; y += 1) {
+        object[`${y}`] = `${i} - ${y}`
+        if (i === 3 && y === 2) {
+            cells.push({
+                name: `${y}`,
+                value: `${i} - ${y}`,
+                style: {},
+            })
+        } else if (i === 8 && y === 2) {
+            cells.push({
+                name: `${y}`,
+                value: `${i} - ${y}`,
+                style: {},
+            })
+        } else {
+            cells.push({
+                name: `${y}`,
+                value: `${i} - ${y}`,
+            })
+        }
+    }
+    rows.push({
+        height: 35,
+        key: `${i}`,
+        object,
+        cells,
+    })
+}
+
+const HeaderResizable = () => {
+    const [columns, setColumns] = useState<Array<Column<unknown>>>(tempColumns)
+    return (
+        <AutoSize>
+            {(width) => (
+                <DataGrid<unknown>
+                    rows={rows}
+                    width={width}
+                    height={500}
+                    columns={columns}
+                    onHeaderResizable={(changeColumns) => {
+                        setColumns(changeColumns)
+                    }}
+                />
+            )}
+        </AutoSize>
+    )
+}
+
+export default HeaderResizable
